51d9df0464456961aa5f8709e362f10e13c58c5c
ReflectionsQAAutomation <automationqa@reflectionsinfos.com>
2024-03-25 15:50:08 +0530

    Commiting  product purchase sample testcase

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
     },
+    experimentalStudio: false,
+
+    specPattern: ["cypress/Component/**/**/*.js", "cypress/e2e/**/**/*.js"],
+    setupNodeEvents(on, config) {
+     // set up the events
+    },
+    hideXHRInCommandLog: true,
+    video: true,
+    videoCompression: false,
   },
 });
